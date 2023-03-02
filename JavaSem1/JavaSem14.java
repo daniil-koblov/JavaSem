b8f7561b7e6ba3be7bc67750c890ee356e8b2480
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-02 22:45:11 +0300

    frist add JavaSem14.java

--- JavaSem1/JavaSem14.java
+++ JavaSem1/JavaSem14.java
@@ -1,18 +1,23 @@
 package JavaSem1;
 
+import java.util.Arrays;
+
 public class JavaSem14 {
     public static void main(String[] args){
-        int[] arr = {3, 2, 2, 3};
+        int[] arr = {3, 2, 2, 3, 3, 3, 3};
         int a = arr.length - 1;
         int b = 3;
-        for (int i = 0; i < arr.length / 2; i++) {
+        for (int i = 0; i < a; i++) {
             if(arr[i] == b){
+                while(arr[i] == arr[a]){
+                    a --;
+                }
                 int c = arr[i];
                 arr[i] = arr[a];
                 arr[a] = c;
             }
             a -= 1;
-        }
-        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
     }
+}
 }
