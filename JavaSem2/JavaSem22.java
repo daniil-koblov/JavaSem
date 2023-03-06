96fc8ff08d9d6ee36cfcafd6dff947e5678328b0
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-06 21:36:17 +0300

    first add JavaSem22.java

--- JavaSem2/JavaSem22.java
+++ JavaSem2/JavaSem22.java
@@ -1,5 +1,19 @@
 package JavaSem2;
 
 public class JavaSem22 {
-    
+    public static void main(String[] args) {
+        String str = System.console().readLine().replace(" ", "").toLowerCase();
+        System.out.println(polindromeCheck(str));
+    }
+    public static boolean polindromeCheck(String str) {
+        char[] chars = str.toCharArray();
+        boolean check = true;
+        for (int i = 0; i < chars.length / 2; i++) {
+            if (chars[i] != chars[chars.length - 1 - i]) {
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
 }
