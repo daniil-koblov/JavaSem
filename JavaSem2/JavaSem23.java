7e16c52626805ddf033a104be81dfad06a7a9fa4
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-06 22:18:53 +0300

    first add JavaSem23.java

--- JavaSem2/JavaSem23.java
+++ JavaSem2/JavaSem23.java
@@ -1,5 +1,38 @@
 package JavaSem2;
 
-public class JavaSem23 {
-    
+import java.io.File;
+import java.io.FileWriter;
+
+// написать Test 100 раз в файл 
+public class JavaSem23{
+    public static void main(String[] args) {
+        String str = "Test\n";
+        StringBuilder strOutput = new StringBuilder();
+        for (int i = 0; i < 100; i++){
+            strOutput.append(str);
+        }
+        System.out.println(saveInFile(strOutput.toString()));
+    }
+
+    public static boolean saveInFile(String inputString) {
+        try {
+            String pathProject = System.getProperty("user.dir");
+            String pathFile = pathProject.concat("/save.txt");
+            File fileToSave = new File(pathFile);
+            try {
+                fileToSave.canWrite();                
+            } catch (Exception e) {
+                fileToSave.createNewFile();  
+            }
+            FileWriter writeToFile = new FileWriter(pathFile);
+            writeToFile.write(inputString);
+            writeToFile.close();
+        } catch (Exception e) {
+            System.out.println("catch");
+        } finally {
+            System.out.println("finally");
+        }
+
+        return true;
+    }
 }
