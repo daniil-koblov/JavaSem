7952226efc539ea39184515f9682c885899de89a
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-09 21:26:39 +0300

    first add JavaSem32.java

--- JavaSem3/JavaSem32.java
+++ JavaSem3/JavaSem32.java
@@ -1,5 +1,17 @@
 package JavaSem3;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class JavaSem32 {
-    
+    public static void main(String[] args) {
+        String[] preList = {"Mercury", "Earth", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", 
+                        "Neptune", "Pluto", "Venus", "Mars", "Mars", "Jupiter"};
+        List<String>planets = Arrays.asList(preList);
+        System.out.println(planets);
+        Collections.sort(planets);
+        System.out.println(planets);
+    }
 }
+
