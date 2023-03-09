7c022261857b6535c89bfd4ec978e09d14d836e1
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-09 21:48:58 +0300

    first add JavaSem33.java

--- JavaSem3/JavaSem33.java
+++ JavaSem3/JavaSem33.java
@@ -1,5 +1,25 @@
 package JavaSem3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JavaSem33 {
-    
+    public static void main(String[] args) {
+        List<String> strNum = new ArrayList<>();
+        strNum.add("apple");
+        strNum.add("3");
+        strNum.add("gray");
+        strNum.add("sky");
+        strNum.add("24");
+        System.out.println(strNum);
+        List<String> newStrNum = new  ArrayList<>();
+        for (String item : strNum){
+            try{
+                Integer.parseInt(item);
+            }catch (Exception e){
+                newStrNum.add(item);
+            }
+        }
+        System.out.println((newStrNum));
+    }
 }
