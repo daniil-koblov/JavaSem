1253016d20bb1e0729e7bf71042068678d33bc58
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-09 20:55:01 +0300

    first add JavaSem31.java

--- JavaSem3/JavaSem31.java
+++ JavaSem3/JavaSem31.java
@@ -1,5 +1,18 @@
+
 package JavaSem3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class JavaSem31 {
-    
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            numbers.add((int)(Math.random() * (20 - 0)) + 1);
+        }
+        System.out.println(numbers);
+        Collections.sort(numbers);
+        System.out.println(numbers);
+    }
 }
