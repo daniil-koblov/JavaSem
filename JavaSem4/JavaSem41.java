3d8e8bab70aed7c688731e78107409bc6cc2f1f7
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-16 16:36:06 +0300

    first add JavaSem41.java

--- JavaSem4/JavaSem41.java
+++ JavaSem4/JavaSem41.java
@@ -1,5 +1,33 @@
 package JavaSem4;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class JavaSem41 {
-    
+    public static void main(String[] args) {
+        String[] inputArr;
+        String input = "text~0";
+        Scanner sc = new Scanner(System.in);
+        LinkedList<String> Hranilishe = new LinkedList<>();
+        while (!input.equals("exit")) {
+            System.out.println("Введите текст");
+            input = sc.nextLine();
+            inputArr = input.split("~");
+
+            if (!inputArr[0].equals("print")) {
+                if (Hranilishe.size() < Integer.parseInt(inputArr[1])) {
+                    for (int i = Hranilishe.size(); i < Integer.parseInt(inputArr[1]); i++) {
+                        Hranilishe.add(null);
+                    }
+
+                }
+                Hranilishe.add(Integer.parseInt(inputArr[1]), inputArr[0]);
+                System.out.println(Hranilishe);
+            }
+            else {
+                System.out.println(Hranilishe.get(Integer.parseInt(inputArr[1])));
+                Hranilishe.remove(Integer.parseInt(inputArr[1]));
+            }
+        }
+    }
 }
