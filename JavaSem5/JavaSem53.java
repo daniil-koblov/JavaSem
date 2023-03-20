50bf5198c5b909395c793c2b84cd1173d1f4950d
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-20 17:51:18 +0300

    first add JavaSem53.java

--- JavaSem5/JavaSem53.java
+++ JavaSem5/JavaSem53.java
@@ -1,5 +1,40 @@
+//Перевод числа из римского формата в арабский.
 package JavaSem5;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class JavaSem53 {
-    
+    public static void main(String[] args) {
+        Map<Character, Integer> convert = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+
+        convert.put('I', 1);
+        convert.put('V', 5);
+        convert.put('X', 10);
+        convert.put('L', 50);
+        convert.put('C', 100);
+        convert.put('D', 500);
+        convert.put('M', 1000); 
+
+        String inputStr = "XVIII";
+        char ch;
+        int sum = 0;
+        for(int i = 0; i < inputStr.length(); i++){
+            ch = inputStr.charAt(i);
+            list.add(convert.get(ch));
+        }
+        for(int i = 0; i < list.size() - 1; i++){
+            if(list.get(i) < list.get(i + 1)){
+                sum -= list.get(i);
+            }
+            else{
+                sum += list.get(i);
+            }
+        }
+        sum += list.get(list.size() - 1);
+        System.out.println(sum);
+    }
 }
